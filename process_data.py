da5a72bd9cbc88bc072651542fb082b0a2a7d5b8
MiYinWt <330324wutao@gmail.com>
2025-04-25 18:10:51 +0800

    changes

--- process_data.py
+++ process_data.py
@@ -2,7 +2,10 @@ import pandas as pd
 
 df = pd.read_csv('./data/BindingDB_All.tsv',sep='\t', low_memory=False)
 
-smiles = 'Ligand SMILES'
-TargetName = 'Target Name'
-IC='IC50 (nM)'
+smiles_col = 'Ligand SMILES'
+TargetName_col = 'Target Name'
+IC50_col = 'IC50 (nM)'
 
+#Filter Targets
+
+df_filtered = df[df[TargetName_col].str.contains('Fibroblast Growth Factor Receptor 1')]
