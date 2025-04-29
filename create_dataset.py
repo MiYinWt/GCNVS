92f5291c2bb266a165e6bd84770d3ae4495308de
MiYinWt <330324wutao@gmail.com>
2025-04-29 19:55:30 +0800

    encoding change

--- create_dataset.py
+++ create_dataset.py
@@ -5,7 +5,8 @@ from torch_geometric.transforms import Compose
 from rdkit import Chem
 from rdkit.Chem import MolFromSmiles
 import networkx as nx
-import numpy as np
+
+from utils import *
 
 class VSDataset(Dataset):
     def __init__(self, data_path, transform=None):
@@ -22,13 +23,7 @@ class VSDataset(Dataset):
             data = self.transform(data)
         return data
 
-def atom_features(atom):
-    # return np.array(one_of_k_encoding_unk(atom.GetSymbol(),['C', 'N', 'O', 'S', 'F', 'P', 'Cl', 'Br', 'Mg', 'Na','Ca', 'Fe', 'As', 'Al', 'I', 'B', 'V', 'K', 'Tl', 'Yb','Sb', 'Sn', 'Ag', 'Pd', 'Co', 'Se', 'Ti', 'Zn', 'H','Li', 'Ge', 'Cu', 'Au', 'Ni', 'Cd', 'In', 'Mn', 'Zr','Cr', 'Pt', 'Hg', 'Pb', 'Unknown']) +
-    #                 one_of_k_encoding(atom.GetDegree(), [0, 1, 2, 3, 4, 5, 6,7,8,9,10]) +
-    #                 one_of_k_encoding_unk(atom.GetTotalNumHs(), [0, 1, 2, 3, 4, 5, 6,7,8,9,10]) +
-    #                 one_of_k_encoding_unk(atom.GetImplicitValence(), [0, 1, 2, 3, 4, 5, 6,7,8,9,10]) +
-    #                 [atom.GetIsAromatic()])
-    return 0
+
 
 def smile_to_graph(smile):
     mol = Chem.MolFromSmiles(smile)
--- utils.py
+++ utils.py
@@ -1,7 +1,29 @@
 from math import sqrt
 from scipy import stats
 import numpy as np
+from rdkit import Chem
+from rdkit.Chem import MolFromSmiles
 
+
+def atom_features(atom):
+    return np.array(one_of_k_encoding_unk(atom.GetSymbol(),['C', 'N', 'O', 'S', 'F', 'P', 'Cl', 'Br', 'I', 'B', 'H', 'Unknown']) +
+                    one_of_k_encoding(atom.GetDegree(), [0, 1, 2, 3, 4, 5, 6]) +
+                    one_of_k_encoding_unk(atom.GetTotalNumHs(), [0, 1, 2, 3, 4, 5]) +
+                    one_of_k_encoding_unk(atom.GetTotalValence(), [0, 1, 2, 3, 4, 5, 6]) +
+                    one_of_k_encoding_unk(atom.GetHybridization(),['SP', 'SP2', 'SP3', 'SP3D', 'SP3D2','Unknown']) +
+                    [atom.atom.IsInRing()]+
+                    [atom.GetIsAromatic()])
+
+def one_of_k_encoding(x, allowable_set):
+    if x not in allowable_set:
+        raise Exception("input {0} not in allowable set{1}:".format(x, allowable_set))
+    return list(map(lambda s: x == s, allowable_set))
+
+def one_of_k_encoding_unk(x, allowable_set):
+    """Maps inputs not in the allowable set to the last element."""
+    if x not in allowable_set:
+        x = allowable_set[-1]
+    return list(map(lambda s: x == s, allowable_set))
 
 def rmse(y,f):
     rmse = sqrt(((y - f)**2).mean(axis=0))
