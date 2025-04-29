99d9428f7e32bad5fa4a9a2a279f72ff1f188b36
MiYinWt <330324wutao@gmail.com>
2025-04-29 16:33:01 +0800

    encoding

--- create_dataset.py
+++ create_dataset.py
@@ -2,6 +2,10 @@ import torch
 from torch_geometric.data import Dataset, DataLoader
 from torch_geometric import data as DATA
 from torch_geometric.transforms import Compose
+from rdkit import Chem
+from rdkit.Chem import MolFromSmiles
+import networkx as nx
+import numpy as np
 
 class VSDataset(Dataset):
     def __init__(self, data_path, transform=None):
@@ -17,3 +21,31 @@ class VSDataset(Dataset):
         if self.transform:
             data = self.transform(data)
         return data
+
+def atom_features(atom):
+    # return np.array(one_of_k_encoding_unk(atom.GetSymbol(),['C', 'N', 'O', 'S', 'F', 'P', 'Cl', 'Br', 'Mg', 'Na','Ca', 'Fe', 'As', 'Al', 'I', 'B', 'V', 'K', 'Tl', 'Yb','Sb', 'Sn', 'Ag', 'Pd', 'Co', 'Se', 'Ti', 'Zn', 'H','Li', 'Ge', 'Cu', 'Au', 'Ni', 'Cd', 'In', 'Mn', 'Zr','Cr', 'Pt', 'Hg', 'Pb', 'Unknown']) +
+    #                 one_of_k_encoding(atom.GetDegree(), [0, 1, 2, 3, 4, 5, 6,7,8,9,10]) +
+    #                 one_of_k_encoding_unk(atom.GetTotalNumHs(), [0, 1, 2, 3, 4, 5, 6,7,8,9,10]) +
+    #                 one_of_k_encoding_unk(atom.GetImplicitValence(), [0, 1, 2, 3, 4, 5, 6,7,8,9,10]) +
+    #                 [atom.GetIsAromatic()])
+    return 0
+
+def smile_to_graph(smile):
+    mol = Chem.MolFromSmiles(smile)
+    
+    c_size = mol.GetNumAtoms()
+    
+    features = []
+    for atom in mol.GetAtoms():
+        feature = atom_features(atom)
+        features.append( feature / sum(feature) )
+
+    edges = []
+    for bond in mol.GetBonds():
+        edges.append([bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()])
+    g = nx.Graph(edges).to_directed()
+    edge_index = []
+    for e1, e2 in g.edges:
+        edge_index.append([e1, e2])
+        
+    return c_size, features, edge_index
---
+++ utils.py
@@ -0,0 +1,20 @@
+from math import sqrt
+from scipy import stats
+import numpy as np
+
+
+def rmse(y,f):
+    rmse = sqrt(((y - f)**2).mean(axis=0))
+    return rmse
+
+def mse(y,f):
+    mse = ((y - f)**2).mean(axis=0)
+    return mse
+
+def pearson(y,f):
+    rp = np.corrcoef(y, f)[0,1]
+    return rp
+
+def spearman(y,f):
+    rs = stats.spearmanr(y, f)[0]
+    return rs
